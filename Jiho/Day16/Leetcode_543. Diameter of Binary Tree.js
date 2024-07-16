5d704652e540270b11eab3ebdd9ab4b25fc3bef2
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-17 01:25:30 +0900

    Refactor usability tests.

---
+++ Jiho/Day16/Leetcode_543. Diameter of Binary Tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+  let max = 0;
+  function getMaxDiameter(node) {
+    // #1. 마지막에 도착했을 때
+    if (node.left === null && node.right === null) {
+      return 0;
+    }
+    // #2. 마지막이 아닐 때
+    const leftDiameter = node.left === null ? 0 : 1 + getMaxDiameter(node.left);
+    const rightDiameter = node.right === null ? 0 : 1 + getMaxDiameter(node.right);
+    const diameter = leftDiameter + rightDiameter;
+    max = Math.max(diameter, max);
+    console.log("node: ", node, "에서의 max: ", max);
+    return Math.max(leftDiameter, rightDiameter);
+  }
+  getMaxDiameter(root);
+  return max;
+};
