b891ba7a0b1e50df1af8b113b3c6ed8453935fa5
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2024-07-30 21:36:59 +0900

    Day30 2Q 지호

---
+++ Jiho/Day30/Leetcode_4. Median of Two Sorted Arrays.js
@@ -0,0 +1,16 @@
+var findMedianSortedArrays = function (nums1, nums2) {
+  let arr = [...nums1, ...nums2];
+  arr.sort((a, b) => a - b);
+  const len = arr.length;
+  // 홀수
+  if (len % 2) {
+    return arr[Math.floor(len / 2)];
+  }
+  // 짝수
+  else {
+    const mid = Math.floor(len / 2);
+    return (arr[mid] + arr[mid - 1]) / 2;
+  }
+};
+
+console.log(findMedianSortedArrays([1, 3], [2, 4, 5]));
